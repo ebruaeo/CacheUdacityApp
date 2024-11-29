42664e51687b7c52afaa5116eb6b9751fdcc5f0d
ebruaeo <ebruaeo@hotmail.com>
2024-11-29 17:26:20 +0300

    using the repository in the viewmodel

--- app/src/main/java/com/example/behindthescenes/repository/VideosRepository.kt
+++ app/src/main/java/com/example/behindthescenes/repository/VideosRepository.kt
@@ -1,12 +1,21 @@
 package com.example.behindthescenes.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
 import com.example.behindthescenes.database.VideosDatabase
+import com.example.behindthescenes.database.asDomainModel
+import com.example.behindthescenes.domain.Video
 import com.example.behindthescenes.network.Network
 import com.example.behindthescenes.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class VideosRepository(private val database: VideosDatabase) {
+
+    val videos: LiveData<List<Video>> =
+        database.videoDao.getVideos().map {
+            it.asDomainModel()
+        }
 
     suspend fun refreshVideos(){
         withContext(Dispatchers.IO) {
--- app/src/main/java/com/example/behindthescenes/viewmodels/DevByteViewModel.kt
+++ app/src/main/java/com/example/behindthescenes/viewmodels/DevByteViewModel.kt
@@ -2,9 +2,11 @@ package com.example.behindthescenes.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.*
+import com.example.behindthescenes.database.getDatabase
 import com.example.behindthescenes.domain.Video
 import com.example.behindthescenes.network.Network
 import com.example.behindthescenes.network.asDomainModel
+import com.example.behindthescenes.repository.VideosRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -13,24 +15,16 @@ import java.io.IOException
 
 class DevByteViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _playlist = MutableLiveData<List<Video>>()
-
-    val playlist: LiveData<List<Video>>
-        get() = _playlist
-
+   private val database = getDatabase(application)
+    private val videosRepository = VideosRepository(database)
 
     init {
-        refreshDataFromNetwork()
-    }
-
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
-        try {
-            val playlist = Network.devbytes.getPlaylist().await()
-            _playlist.postValue(playlist.asDomainModel())
-        } catch (networkError: IOException) {
-
+        viewModelScope.launch {
+            videosRepository.refreshVideos()
         }
     }
+
+    val playlist = videosRepository.videos
 
     class Factory(val app: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
