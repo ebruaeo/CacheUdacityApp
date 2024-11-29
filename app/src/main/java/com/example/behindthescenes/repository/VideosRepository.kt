8d84f7431d3f196db54832ebafa9dc85fa17dae4
ebruaeo <ebruaeo@hotmail.com>
2024-11-29 17:20:45 +0300

    Building the repository

--- app/src/main/java/com/example/behindthescenes/repository/VideosRepository.kt
+++ app/src/main/java/com/example/behindthescenes/repository/VideosRepository.kt
@@ -1,4 +1,17 @@
 package com.example.behindthescenes.repository
 
-class VideosRepository {
+import com.example.behindthescenes.database.VideosDatabase
+import com.example.behindthescenes.network.Network
+import com.example.behindthescenes.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class VideosRepository(private val database: VideosDatabase) {
+
+    suspend fun refreshVideos(){
+        withContext(Dispatchers.IO) {
+            val playlist= Network.devbytes.getPlaylist().await()
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+        }
+    }
 }
