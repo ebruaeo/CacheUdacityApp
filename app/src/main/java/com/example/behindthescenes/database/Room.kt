bb7bc5321fccb4e66ffe154071ed1b5259463630
ebruaeo <ebruaeo@hotmail.com>
2024-11-29 13:29:57 +0300

    Adding a VideoDao

--- app/src/main/java/com/example/behindthescenes/database/Room.kt
+++ app/src/main/java/com/example/behindthescenes/database/Room.kt
@@ -1,4 +1,16 @@
 package com.example.behindthescenes.database
 
-class Room {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.selects.select
+
+@Dao
+interface VideoDao {
+    @Query(value = "select* from databasevideo")
+    fun getVideos(): List<DatabaseVideo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
 }
