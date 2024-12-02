e392df4f9507a4cf3a928c74532fa913f20cb06a
ebruaeo <ebruaeo@hotmail.com>
2024-12-02 15:21:12 +0300

    Work manager for background

--- app/src/main/java/com/example/behindthescenes/DevByteApplication.kt
+++ app/src/main/java/com/example/behindthescenes/DevByteApplication.kt
@@ -1,14 +1,40 @@
 package com.example.behindthescenes
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.behindthescenes.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 
 class DevByteApplication : Application() {
-
+    val applicationScope = CoroutineScope(Dispatchers.Default)
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+        delayedInit()
     }
+
+    private fun delayedInit() = applicationScope.launch {
+        setupRecurringWork()
+    }
+
+    private fun setupRecurringWork() {
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+    }
+
 }
--- app/src/main/java/com/example/behindthescenes/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/behindthescenes/work/RefreshDataWork.kt
@@ -1,4 +1,26 @@
 package com.example.behindthescenes.work
 
-class RefreshDataWork {
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.behindthescenes.database.getDatabase
+import com.example.behindthescenes.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters):
+    CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
 }
